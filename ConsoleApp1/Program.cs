30825d27202ab0cade6d7dcd9a45a7e7ce560861
Alvin Moreno <alvin.moreno@avanade.com>
2021-02-11 16:30:02 +1100

    xf

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,9 +4,13 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public static string password = "abc123";
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            Console.WriteLine("Hello World! this should trigger a scan alert");
         }
     }
 }
